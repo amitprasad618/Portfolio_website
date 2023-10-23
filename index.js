c10f07de065ff8ea54250244754f72d4dc16f7b5
Amit  Prasad <amit.prasad@wavelabs.ai>
2023-10-23 15:47:13 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,101 @@
+var bttn = document.getElementById("btn");
+
+bttn.addEventListener("click", function () {
+
+  var linkUrl = "https://drive.google.com/drive/u/0/folders/1xmHlR8XuRN5qWyqyohXZokvNv2X3KSVB";
+
+
+  window.open(linkUrl, "_blank");
+});
+
+
+
+// JavaScript to open and close the dialog box
+const openDialogButton1 = document.getElementById('open-dialog-button1');
+const dialogOverlay1 = document.getElementById('dialog-overlay1');
+const closeDialogButton1 = document.getElementById('close-dialog-button1');
+
+openDialogButton1.addEventListener('click', () => {
+    dialogOverlay1.style.display = 'block';
+    closeDialogButton1.style.display = 'block'; // Show the close button
+});
+
+closeDialogButton1.addEventListener('click', () => {
+    dialogOverlay1.style.display = 'none';
+    closeDialogButton1.style.display = 'none'; // Hide the close button
+});
+
+
+const openDialogButton2 = document.getElementById('open-dialog-button2');
+const dialogOverlay2 = document.getElementById('dialog-overlay2');
+const closeDialogButton2 = document.getElementById('close-dialog-button2');
+
+openDialogButton2.addEventListener('click', () => {
+    dialogOverlay2.style.display = 'block';
+    closeDialogButton2.style.display = 'block'; // Show the close button
+});
+
+closeDialogButton2.addEventListener('click', () => {
+    dialogOverlay2.style.display = 'none';
+    closeDialogButton2.style.display = 'none'; // Hide the close button
+});
+
+
+const openDialogButton3 = document.getElementById('open-dialog-button3');
+const dialogOverlay3 = document.getElementById('dialog-overlay3');
+const closeDialogButton3 = document.getElementById('close-dialog-button3');
+
+openDialogButton3.addEventListener('click', () => {
+    dialogOverlay3.style.display = 'block';
+    closeDialogButton3.style.display = 'block'; // Show the close button
+});
+
+closeDialogButton3.addEventListener('click', () => {
+    dialogOverlay3.style.display = 'none';
+    closeDialogButton3.style.display = 'none'; // Hide the close button
+});
+
+
+const openDialogButton4 = document.getElementById('open-dialog-button4');
+const dialogOverlay4 = document.getElementById('dialog-overlay4');
+const closeDialogButton4 = document.getElementById('close-dialog-button4');
+
+openDialogButton4.addEventListener('click', () => {
+    dialogOverlay4.style.display = 'block';
+    closeDialogButton4.style.display = 'block'; // Show the close button
+});
+
+closeDialogButton4.addEventListener('click', () => {
+    dialogOverlay4.style.display = 'none';
+    closeDialogButton4.style.display = 'none'; // Hide the close button
+});
+
+
+const openDialogButton5 = document.getElementById('open-dialog-button5');
+const dialogOverlay5 = document.getElementById('dialog-overlay5');
+const closeDialogButton5 = document.getElementById('close-dialog-button5');
+
+openDialogButton5.addEventListener('click', () => {
+    dialogOverlay5.style.display = 'block';
+    closeDialogButton5.style.display = 'block'; // Show the close button
+});
+
+closeDialogButton5.addEventListener('click', () => {
+    dialogOverlay5.style.display = 'none';
+    closeDialogButton5.style.display = 'none'; // Hide the close button
+});
+
+
+const openDialogButton6 = document.getElementById('open-dialog-button6');
+const dialogOverlay6 = document.getElementById('dialog-overlay6');
+const closeDialogButton6 = document.getElementById('close-dialog-button6');
+
+openDialogButton6.addEventListener('click', () => {
+    dialogOverlay6.style.display = 'block';
+    closeDialogButton6.style.display = 'block'; // Show the close button
+});
+
+closeDialogButton6.addEventListener('click', () => {
+    dialogOverlay6.style.display = 'none';
+    closeDialogButton6.style.display = 'none'; // Hide the close button
+});
